a5fce01c9fc9a7ac13014a13fc3819a19ead2c22
Ivan Korostelev <diagon555@gmail.com>
2016-01-23 13:02:45 +0300

    Heaters without link to TempSensors and Relay in add commad

---
+++ src/Heaters.cpp
@@ -0,0 +1,153 @@
+#include "Heaters.h"
+
+Heater::Heater(String name, Relay *relay, TempSensor *tempsensor, int Tset, int Tsup, boolean state)
+	:iNamable(name)
+{
+	this->relay = relay;
+	this->tempsensor = tempsensor;
+	this->Tset = Tset;
+	this->Tsup = Tsup;
+	this->heat_state = state;
+}
+
+void Heater::check()
+{
+	//
+}
+
+//Heaters
+Heaters* Heaters::instance;
+
+Heaters::Heaters()
+	:iSerializable(1+((1+1+1+1+1+8)*MAX_COUNT_HEATERS))
+{
+	Heaters::instance = this;
+}
+
+void Heaters::Serialize()
+{
+	int addr = start_address;
+	EEPROM.update(addr++, count());
+	for (link t = head; t != NULL; t = t->next) {
+		addr += 5;
+		String name = t->item->GetName();
+		for(int i=0; i < 8; i++)
+		{
+			char ch;
+			if(i < name.length())
+				ch = name[i];
+			else
+				ch = 0;
+			
+			EEPROM.update(addr++, ch);
+		}
+	}
+
+}
+
+void Heaters::Deserialize()
+{
+	int addr = start_address;
+	uint8_t count = EEPROM.read(addr++);
+	if(count > MAX_COUNT_HEATERS) count = 0;
+	for (uint8_t idx = 0; idx < count; idx++) {
+		addr += 5;
+		
+		char name[9];
+		for(int i=0; i < 8; i++)
+		{
+			name[i] = EEPROM.read(addr++);
+		}
+		name[8] = 0;
+		
+		//this->push(new Heater(String(name)));
+	}	
+}
+
+String Heaters::command(Command * command)
+{
+	String str;
+	
+	String cmd = command->Next();
+	String cmd_2 = command->Next();
+	
+	if(cmd == "help") {
+		str += "heater list | add <name> <relay_num> <tempsensor_num> | <name> (on|off|set <temp>|set tsup <temp>) \n\r";
+	} else if(cmd == "add") {
+		//Relay relay = relays->
+		//Add(new Heater(cmd_2, relay, tempsensor));
+		//Serialize();
+	} else if(cmd == "list" || cmd == "status") {
+		int i = 1;
+		for (link t = head; t != NULL; t = t->next) {
+			String state;
+			if(t->item->GetStatus())
+				state = "on";
+			else
+				state = "off";
+			
+			str += String(i) + ": " + t->item->GetName() + " " + t->item->GetTemp() + "(" + t->item->GetSetTemp() + ") " + state + "\n\r";
+			i++;
+		}
+	} else {
+		Heater *heater = GetByName(cmd);
+		if(heater)
+		{
+			if(cmd_2 == "on"){
+				heater->On();
+				str += "OK ON";
+			} else if(cmd_2 == "off") {
+				heater->Off();
+				str += "OK OFF";
+			} else {
+				String state;
+				if(heater->GetStatus())
+					state = "on";
+				else
+					state = "off";
+				str += String("Heater ") + heater->GetName() + " " + heater->GetTemp() + "(" + heater->GetSetTemp() + ") " + state + "\n\r";
+			}
+		}
+		else
+		{
+			str += "unknown command";
+		}
+	}
+	
+	return str;	
+
+}
+
+uint8_t Heaters::GetNum(Heater *heater)
+{
+	uint8_t i = 255;
+	for (link t = head; t != NULL; t = t->next) {
+		if(t->item == heater) return i;
+		i++;
+	}
+	
+	return i;
+}
+
+Heater *Heaters::GetByName(String name)
+{
+	for (link t = head; t != NULL; t = t->next) {
+		if(t->item->GetName() == name)
+		{
+			return t->item;
+		}
+	}	
+}
+
+void Heaters::_check()
+{
+	for (link t = head; t != NULL; t = t->next) {
+		t->item->check();
+	}
+
+}
+
+void Heaters::check() //Sheduler every 1000ms
+{
+	Heaters::instance->_check();
+}
--- src/Heaters.h
+++ src/Heaters.h
@@ -1,6 +1,53 @@
 #ifndef Heaters_h
 #define Heaters_h
 
+#include "Relay.h"
+#include "TempSensors.h"
 
+#define HEAT_OFF -127
+#define MAX_COUNT_HEATERS 12
+
+
+class Heater: public iNamable
+{
+	Relay *relay;
+	TempSensor *tempsensor;
+	
+	int Tset;
+	int Tsup;
+	boolean heat_state;
+public:
+	Heater(String name, Relay *relay, TempSensor *tempsensor, int Tset = 23, int Tsup = HEAT_OFF, boolean state = false);
+	
+	void SetTemp(int temp){Tset = temp;};
+	void SetTempSupport(int temp){Tsup = temp;};
+	
+	void On(){heat_state = true;}
+	void Off(){heat_state = false;}
+	
+	void OffForce(){Tset = HEAT_OFF; };
+	void OffSupportTemp(){Tsup = HEAT_OFF; };
+	
+	int GetTemp(){return tempsensor->GetTemp();};
+	int GetSetTemp(){return Tset;};
+	boolean GetStatus(){return heat_state;};
+	
+	void check();
+};
+
+class Heaters: public iSerializable, public List<Heater*>
+{
+public:
+	static Heaters *instance;
+	Heaters();
+	virtual void Serialize();
+	virtual void Deserialize();
+	String command(Command * command);
+	void Add(Heater *heater){push(heater);};
+	uint8_t GetNum(Heater *);
+	Heater *GetByName(String name);
+	void _check();
+	static void check(); //Sheduler every 1000ms (get temp from sensors)
+};
 
 #endif
